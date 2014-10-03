ac210bfaf76f060a8520146f6d682b359a8608af
damien@fiontar.net <damien@fiontar.net>
2014-10-03 23:07:43 +0200

    testing dual ports - again

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
-var express = require('express');
-var app = express();
-
-var port1 = process.env.PORT || 1337;
-var port2 = process.env.PORT || 1338;
-
 var http = require('http');
-
+var port1 = process.env.PORT || 1337;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello World\n');
 }).listen(port1);
 
+var express = require('express');
+var app = express();
+
+var port2 = process.env.PORT || 1338;
 app.get('/hello.txt', function(req, res){
 	res.send('Hello World from ExpressJS (via Azure)');
 });
