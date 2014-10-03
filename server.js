d9a8f07a63a32f48d864b0833132cc1ba43850c4
damien@fiontar.net <damien@fiontar.net>
2014-10-03 23:18:44 +0200

    added express on 8080

--- server.js
+++ server.js
@@ -14,6 +14,6 @@ app.get('/hello.txt', function(req, res){
 	res.send('Hello World from ExpressJS (via Azure)');
 });
 
-var server = app.listen(8080, function() {
+var server = app.listen(port, function() {
     console.log('Listening on port %d', server.address().port);
 });
