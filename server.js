80063f656384183d0bd25c66366843966d877ad3
damien@fiontar.net <damien@fiontar.net>
2014-10-03 22:58:01 +0200

    changed port

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var app = express();
 
+var port = process.env.PORT || 1337;
+
 app.get('/hello.txt', function(req, res){
 	res.send('Hello World from ExpressJS (via Azure)');
 });
 
-var server = app.listen(3000, function() {
+var server = app.listen(port, function() {
     console.log('Listening on port %d', server.address().port);
 });
