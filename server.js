c0b6c9344145d93cea34b9fe440bf5efcf3012e2
damien@fiontar.net <damien@fiontar.net>
2014-10-03 23:14:35 +0200

    removed express

--- server.js
+++ server.js
@@ -5,14 +5,3 @@ http.createServer(function(req, res) {
   res.end('Hello World\n');
 }).listen(port1);
 
-var express = require('express');
-var app = express();
-
-var port = process.env.PORT || 1339;
-app.get('/hello.txt', function(req, res){
-	res.send('Hello World from ExpressJS (via Azure)');
-});
-
-var server = app.listen(port, function() {
-    console.log('Listening on port %d', server.address().port);
-});
