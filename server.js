0f643eb1c2c5607c806707fb6d2cf040723fde54
damien@fiontar.net <damien@fiontar.net>
2014-10-03 23:00:11 +0200

    running http and express side by side

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ var app = express();
 
 var port = process.env.PORT || 1337;
 
+var http = require('http')
+
+http.createServer(function(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World\n');
+}).listen(port);
+
 app.get('/hello.txt', function(req, res){
 	res.send('Hello World from ExpressJS (via Azure)');
 });
