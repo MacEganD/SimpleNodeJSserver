f90774542aed308ea51fc6c25fde98c18c95c1d4
damien@fiontar.net <damien@fiontar.net>
2014-10-03 22:53:27 +0200

    Added Hello World from ExpressJS

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
-var http = require('http')
-var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World from NodeJS via Azure ;-)\n');
-}).listen(port);
+var express = require('express');
+var app = express();
+
+app.get('/hello.txt', function(req, res){
+	res.send('Hello World from ExpressJS (via Azure)');
+});
+
+var server = app.listen(3000, function() {
+    console.log('Listening on port %d', server.address().port);
+});
