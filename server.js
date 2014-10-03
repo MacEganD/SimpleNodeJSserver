f7c1168a3b4fec59376e345ca14e9e2acc0beb10
damien@fiontar.net <damien@fiontar.net>
2014-10-03 23:51:20 +0200

    just running express on 1337

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
-
-
 var express = require('express');
 var app = express();
 
 var port = process.env.PORT || 1337;
 //var port = 8080;
 app.get('/express.txt', function(req, res){
+	res.send('Hello World from ExpressJS (via Azure)');
+});
+
+app.get('/', function(req, res){
 	res.send('Hello World from ExpressJS (via Azure)');
 });
 
